bf6c22082698586b1056a7275010f6b4c78ebdf9
mohan bendegumbali <mohan0311@hotmail.com>
2023-03-25 20:40:23 +0530

    Anagram

---
+++ Anagram.java
@@ -0,0 +1,40 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagram {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first string: ");
+        String str1 = scanner.nextLine();
+        System.out.print("Enter the second string: ");
+        String str2 = scanner.nextLine();
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println(str1 + " and " + str2 + " are anagrams");
+        } else {
+            System.out.println(str1 + " and " + str2 + " are not anagrams");
+        }
+    }
+
+    public static boolean areAnagrams(String str1, String str2) {
+        // Remove whitespace and convert to lowercase
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        // Check if lengths are equal
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        // Convert strings to character arrays and sort
+        char[] str1Array = str1.toCharArray();
+        char[] str2Array = str2.toCharArray();
+        Arrays.sort(str1Array);
+        Arrays.sort(str2Array);
+
+        // Compare sorted arrays
+        return Arrays.equals(str1Array, str2Array);
+    }
+}
